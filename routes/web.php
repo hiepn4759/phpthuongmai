60f5149cd843a963c3cd4b057a2f12a1210384a7
hiepn4759 <hiepn4759@gmail.com>
2019-09-17 15:44:14 +0700

    chinh sua Logout cho dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', 'HomeController@index');
 
 
 //backend routes..................
+Route::get('/logout', 'SuperAdminController@logout');
 Route::get('/admin', 'AdminController@index');
 Route::get('/dashboard', 'AdminController@show_dashboard');
 Route::post('/admin-dashboard', 'AdminController@dashboard');
